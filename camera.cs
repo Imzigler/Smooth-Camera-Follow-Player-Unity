56655e0bada1bd0d3481cd3d3576bc12c07de990
anouar zeklat <77289226+Imzigler@users.noreply.github.com>
2021-12-26 21:53:19 +0100

    Add files via upload

---
+++ camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+	public Transform player;
+	public Vector3 offset;
+	public float smoothspeed;
+	
+	
+	// LateUpdate is called every frame, if the Behaviour is enabled.
+	protected void LateUpdate()
+	{
+		Vector3 diseredposition = player.position + offset;
+		Vector3 smoothedposition = Vector3.Slerp(transform.position,diseredposition,smoothspeed);
+		transform.position = smoothedposition;
+		transform.LookAt(player);
+	}
+}
